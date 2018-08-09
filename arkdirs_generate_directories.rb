2fc594ecd87b5f446174a83927c84cdbd40152ce
Katherine Lynch <katherly@upenn.edu>
2018-08-09 16:49:33 -0400

    First pass creating directories from arks

--- arkdirs_generate_directories.rb
+++ arkdirs_generate_directories.rb
@@ -1,16 +1,46 @@
 #!/usr/bin/ruby
 
+require 'logger'
+require 'todo_runner'
+require 'yaml'
+
+require_relative 'lib/naming'
+
 def invalid_args?
   return true if ARGV[0].nil?
   return true unless File.exist?(ARGV[0])
   return false
 end
 
-def directorify(ark)
-  return ark.to_s.gsub(/[:\/]/, ':' => '+', '/' => '=') unless ark.nil?
-end
-
 abort('Please supply a YML ark manifest.') if invalid_args?
 
 manifest = ARGV[0]
 
+logger = Logger.new(STDOUT)
+logger.level = Logger::INFO
+
+TodoRunner.define do
+  start :create_directory
+
+  task :create_directory, on_fail: :FAIL, next_step: :log do |todo_file|
+    begin
+      data = YAML.load todo_file
+      FileUtils.mkdir_p("#{data[:destination]}/#{Naming.directorify(data[:ark_id])}")
+      true
+    rescue Exception => ex
+      logger.fatal("ERROR: #{ex.message}")
+      false
+    end
+  end
+
+  task :log, on_fail: :FAIL, next_step: :SUCCESS do |todo_file|
+    data = YAML.load todo_file
+    logger.info("Directory created for #{data[:ark_id]}")
+  end
+end
+
+logger.info('Todo Runner initialized, starting run...')
+
+TodoRunner.run(*ARGV)
+
+logger.info('Run complete.')
--- arkdirs_make_todos.rb
+++ arkdirs_make_todos.rb
@@ -1,25 +1,24 @@
 #!/usr/bin/ruby
 
+require 'logger'
 require 'smarter_csv'
 require 'yaml'
+
+require_relative 'lib/naming'
 
 def validate_args!
   raise ArgumentError.new('Please supply a CSV file') if ARGV[0].nil?
   raise ArgumentError.new('Please supply a destination directory') if ARGV[1].nil?
 end
 
-def directorify(ark)
-  return ark.to_s.gsub(/[:\/]/, ':' => '+', '/' => '=') unless ark.nil?
-end
-
-HEADERS = %i{ ark_id file_path }
+HEADERS = %i{ ark_id destination }
 
 def create_todo_file(args = {}, dest_dir)
-  directory_name = directorify(args[:ark_id])
-  todo_base = directory_name
+  directory_name = Naming.directorify(args[:ark_id])
   todo_path = File.join dest_dir, "#{directory_name}.todo"
   data = HEADERS.inject({}) { |h,header| h.merge(header => args[header]) }
   File.open(todo_path, 'w+') { |f| f.puts YAML::dump data }
+  return "#{directory_name}.todo"
 end
 
 validate_args!
@@ -27,6 +26,13 @@ validate_args!
 file = ARGV[0]
 dest_dir = ARGV[1]
 
+logger = Logger.new(STDOUT)
+logger.level = Logger::INFO
+logger.info('Make TODOs started')
+
 SmarterCSV.process(file).each do |row|
-  create_todo_file(row, dest_dir)
+  todo_filename = create_todo_file(row, dest_dir)
+  logger.info("TODO file #{todo_filename} created.")
 end
+
+logger.info("Make TODOs complete.  Files available at #{dest_dir}.")
