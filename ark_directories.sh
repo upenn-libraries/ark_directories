d2c48ad3536da5628cf41b3795c7c321560e2014
Kate Lynch <katherly@upenn.edu>
2018-10-29 17:03:17 -0400

    Finish dockerizing, add nfs management to deploy

--- ark_directories.sh
+++ ark_directories.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 manifest=$1
-docker exec -it arkdirectories_ark_directories_1 mkdir -p /usr/src/app/todos
-docker cp ${manifest} arkdirectories_ark_directories_1:/usr/src/app/.
-docker exec -it arkdirectories_ark_directories_1 bundle exec ruby arkdirs_make_todos.rb ${manifest} todos/
-docker exec -it arkdirectories_ark_directories_1 bash -c "bundle exec ruby arkdirs_generate_directories.rb todos/*.todo"
+docker exec -it ark_directories_ark_directories_1 mkdir -p /usr/src/app/todos
+docker cp ${manifest} ark_directories_ark_directories_1:/usr/src/app/.
+docker exec -it ark_directories_ark_directories_1 bundle exec ruby arkdirs_make_todos.rb ${manifest} todos/
+docker exec -it ark_directories_ark_directories_1 bash -c "bundle exec ruby arkdirs_generate_directories.rb todos/*.todo"
